d22ccf79c857f2c826d630c7ea51bf570ca4f30c
hacketlo <hacketlo@tcd.ie>
2022-11-20 17:31:20 +0000

    first commit

---
+++ src/main/java/org/example/docParser/fbparser.java
@@ -0,0 +1,78 @@
+package org.example.docParser;
+
+import java.io.IOException;
+
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.analysis.core.StopAnalyzer;
+import org.apache.lucene.analysis.core.WhitespaceAnalyzer;
+import org.apache.lucene.analysis.en.EnglishAnalyzer;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.File;
+
+public class fbparser
+{
+    public static ArrayList<Document> parsefb(String input) throws IOException {
+        ArrayList<Document> doclist = new ArrayList<>();
+        int Fcnt = 0;
+        File dir = new File(input);
+        File[] directoryListing = dir.listFiles();
+        for (File child : directoryListing) {
+            if((child.getName()).startsWith("fb")){
+                int Icnt = 0;
+
+
+                ++Fcnt;
+
+                org.jsoup.nodes.Document doc = Jsoup.parse(child,"utf-8");//assuming register.html file in e drive
+
+                Elements ele = doc.getElementsByTag("DOC");
+                Icnt = 0;
+
+                for(Element link : ele){
+                    Document document = new Document();
+
+                    // System.out.println(link.getElementsByTag("DOCNO").text() +"\n");
+                    String id = link.getElementsByTag("DOCNO").text();
+                    document.add(new TextField("docno", id, Field.Store.YES));
+
+                    // System.out.println(link.getElementsByTag("DATE1").text()+"\n");
+                    String date = link.getElementsByTag("DATE1").text();
+                    document.add(new TextField("date", date, Field.Store.YES));
+
+                    // System.out.println(link.getElementsByTag("TI").text()+"\n");
+                    String headline = link.getElementsByTag("TI").text();
+                    document.add(new TextField("headline", headline, Field.Store.YES));
+
+                    // System.out.println(link.getElementsByTag("TEXT").text()+"\n");
+                    String text = link.getElementsByTag("TEXT").text();
+                    document.add(new TextField("text", text, Field.Store.YES));
+
+                    // System.out.println(link.getElementsByAttributeValue("P", "104").text()+"\n");
+                    String pub = link.getElementsByAttributeValue("P", "104").text();
+                    document.add(new TextField("pub", pub, Field.Store.YES));
+                    doclist.add(document);
+                    ++Icnt;
+
+                }
+                System.out.println(child.getName()+ " Docnos "+Integer.toString(Icnt));
+            }
+        }
+        return doclist;
+    }
+}
---
+++ src/main/java/org/example/docParser/frparser.java
@@ -0,0 +1,56 @@
+package org.example.docParser;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.StringField;
+import org.apache.lucene.document.TextField;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
+
+public class frparser {
+    public static ArrayList<Document> parseFR94(String path) throws IOException {
+        ArrayList<Document> doclist = new ArrayList<>();
+        System.out.println("Parsing fr94 documents...");
+        File[] directories = new File(path).listFiles(File::isDirectory);
+        String DOCNO_Str, DATE_Str, AGENCY_Str, SUMMARY_Str, ACTION_Str;
+        for(File directory : directories){
+            File[] files = directory.listFiles();
+            for(File file : files) {
+                org.jsoup.nodes.Document doc = Jsoup.parse(file, null, "");
+                Elements elements = doc.select("DOC");
+                for(Element element : elements){
+                    // ignore child elements
+                    // these elements are not relevant to valuable information
+                    element.select("ADDRESS").remove();
+                    element.select("SIGNER").remove();
+                    element.select("SIGNJOB").remove();
+                    element.select("BILLING").remove();
+                    element.select("FRFILING").remove();
+                    element.select("RINDOCK").remove();
+                    DOCNO_Str = element.select("DOCNO").text();
+                    ACTION_Str = element.select("ACTION").text();
+                    SUMMARY_Str = element.select("SUMMARY").text();
+                    DATE_Str = element.select("DATE").text();
+                    AGENCY_Str = element.select("AGENCY").text();
+                    System.out.println("reading file: " + DOCNO_Str);
+                    Document document = new Document();
+                    document.add(new StringField("docno", DOCNO_Str, Field.Store.YES));
+                    document.add(new TextField("date", DATE_Str, Field.Store.YES));
+                    document.add(new TextField("headline", ACTION_Str, Field.Store.YES));
+                    document.add(new TextField("text", SUMMARY_Str, Field.Store.YES));
+                    document.add(new TextField("pub", AGENCY_Str, Field.Store.YES));
+                    doclist.add(document);
+                }
+            }
+        }
+        System.out.println("Parsing FR94 done...");
+        return doclist;
+    }
+}
